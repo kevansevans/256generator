893bb8420dddf4e116bb1203d005689476957889
Kaelan <kbeevans@gmail.com>
2019-08-21 21:33:43 -0600

    reduced redundant code

--- src/ext/WorkBitmap.hx
+++ src/ext/WorkBitmap.hx
@@ -10,24 +10,21 @@ import openfl.display.BitmapData;
 class WorkBitmap extends Bitmap
 {
 	public var source:Bitmap;
-	public var work:BitmapData;
 	public var hex:Map<String, PixelColor>;
-	public var display:BitmapData;
 	
 	public var alpha_max(null, set):Int;
 	public var alpha_min(null, set):Int;
 	
 	var w_width:Int;
 	var w_height:Int;
-	public function new(_source:BitmapData, _width:Int, _height:Int) 
+	public function new(_source:BitmapData, ?_width:Int, ?_height:Int) 
 	{
 		super();
 		
 		source = new Bitmap(_source);
-		work = _source;
 		
-		w_width = _width;
-		w_height = _height;
+		w_width = _width == null ? source.bitmapData.width : _width;
+		w_height = _height == null ? source.bitmapData.height : _height;
 		
 		hex = new Map();
 		for (w in 0...w_width) {
@@ -37,11 +34,9 @@ class WorkBitmap extends Bitmap
 			}
 		}
 		
-		display = work;
-		this.bitmapData = display;
+		bitmapData = source.bitmapData;
 	}
 	public function reset() {
-		work = source.bitmapData;
 		hex = new Map();
 		for (w in 0...w_width) {
 			for (h in 0...w_height) {
@@ -49,20 +44,14 @@ class WorkBitmap extends Bitmap
 				hex[place] = new PixelColor(source.bitmapData.getPixel32(w, h), w, h);
 			}
 		}
-		display = source.bitmapData;
-		this.bitmapData = display;
-	}
-	
-	function update_work() {
 		for (a in hex) {
-			work.setPixel32(a.x, a.y, a.argb);
+			bitmapData.setPixel32(a.x, a.y, a.argb);
 		}
-		
-		update_display();
 	}
-	
-	function update_display() {
-		this.bitmapData = work;
+	public function update() {
+		for (a in hex) {
+			bitmapData.setPixel32(a.x, a.y, a.argb);
+		}
 	}
 	
 	function set_alpha_max(value:Int):Int 
@@ -80,12 +69,9 @@ class WorkBitmap extends Bitmap
 		}
 		return alpha_min = value;
 	}
-	public function retroApply() {
-		for (a in hex) {
-			work.setPixel32(a.x, a.y, a.argb);
-		}
-		source = new Bitmap(work);
-		display = work;
-		this.bitmapData = display;
+	
+	public function clear() {
+		var empty_bitmap = new BitmapData(w_width, w_height, true, 0);
+		this.bitmapData = empty_bitmap;
 	}
 }
